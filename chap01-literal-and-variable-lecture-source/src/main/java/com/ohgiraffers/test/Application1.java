97b2ae750619e81248b685fc9668542715226787
JungKeunHee <keunhee0920@gmail.com>
2024-08-26 17:44:54 +0900

    [24.08.26 테스트 시험 결과값 포함]

---
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/test/Application1.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.test;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        a = 20;
+        b = 30;
+
+        System.out.println("더하기 결과 : " + (a + b));
+        System.out.println("빼기 결과 : " + (a - b));
+        System.out.println("곱하기 결과 : " + (a * b));
+        System.out.println("나누기한 몫 : " + (a / b));
+        System.out.println("나누기한 나머지 : " + (a % b));
+
+        double c = 12.5;
+        double d = 34.5;
+
+        double f = c * d;
+        double g = c * 4;
+
+        System.out.println("면적 : " + f);
+        System.out.println("둘레 : " + g);
+
+
+    }
+}
