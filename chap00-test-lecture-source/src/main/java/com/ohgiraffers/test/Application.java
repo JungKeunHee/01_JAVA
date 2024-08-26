b980b2b317f69780ac3d81aaa09173186e7ee2e6
JungKeunHee <keunhee0920@gmail.com>
2024-08-26 15:19:50 +0900

    [24.08.26 수업자료]

---
+++ chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.test;
+
+public class Application {
+    // 필기를 해보아요 = 슬래시 두 번은 한 줄 주석처리
+
+    /* 여러줄을 쓸 수 있는 주석
+    하나 둘 셋 넷
+    다섯 여섯 일곱 여덟 = 슬래시 별별 슬래시는 여러줄로 쓸 수 있는 주석처리 */
+
+    public static void main(String[] args) {
+        /* main으로 입력하고 Tab키 or Enter키를 눌러서
+        자동완성 후 해당 영역은 프로그램을 재생할 수 있는 영역을 나타내는 공간이라고 생각하면 됨
+         */
+        System.out.println("Hello world!");
+        System.out.println("안녕하세요");
+        /* sout으로 입력하고 Tab키 or Enter키 눌러서 자동완성 후
+        printin() 안에 "" 값을 넣으면 컴파일 후 해당 값을 보여주게 됨
+         */
+        //System.out.println(안녕하세요);
+    }
+}
