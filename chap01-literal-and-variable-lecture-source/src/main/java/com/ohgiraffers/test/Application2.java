05d3aa4755c7636237ae24411639c63f39929a7c
JungKeunHee <keunhee0920@gmail.com>
2024-08-27 12:58:32 +0900

    [24.08.27 챕터 1번 마무리 및 실습 테스트]

--- chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/test/Application2.java
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/test/Application2.java
@@ -4,7 +4,17 @@ public class Application2 {
 
     public static void main(String[] args) {
 
+        double a;
+        double b;
+        double c;
 
+        a = 80.5;
+        b = 50.6;
+        c = 70.8;
+
+        int abc = (int)(a + b + c);
+        System.out.println("총 점수의 합계 : " + abc);
+        System.out.println("평균 점수 : " + abc/3);
     }
 
 }
