e9ff282f810d4f7d1bedd075a09d2e6bdbdbf049
JungKeunHee <keunhee0920@gmail.com>
2024-08-29 18:09:30 +0900

    [24.08.29 메소드 연습문제 추가]

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Test.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Test.java
@@ -3,25 +3,137 @@ package com.ohgiraffers.section01.method;
 public class Test {
     public static void main(String[] args) {
 
-        int A;
-        int B;
+//        int A;
+//        int B;
+//
+//        A = 2;
+//        B = 6;
+//
+//        String C = (A >= 8) ? "출발 확정" : "출발 보류";
+//        String D = (B >= 4) ? "출발 확정" : "출발 보류";
+//
+//        System.out.println("A 패키지 : " + A + " 출발 : " + C);
+//        System.out.println("B 패키지 : " + B + " 출발 : " + D);
+//
+//        int sum = A + B;
+//
+//        String E = (sum >= 8) ? "출발 확정" : "출발 보류";
+//
+//        System.out.println("기준 미달이였지만 합산한 결과 : " + E);
 
-        A = 2;
-        B = 6;
+//
+//
+//
+//
+//
+//        int형 변수 num1, num2 를 선언하고 num1과 num2가 짝수인지 판별하는 메소드 isEven을  작성하시오.
+//
+//                단, 결과는 참, 거짓으로 출력되며 삼항연산자를 이용하기 바랍니다.
+//
+////        입력 )
+////
+////        num1 = 37
+////
+////        num2 = 28
+////
+////        출력 결과 )
+////
+////        num1 = 거짓
+////
+////        num2 = 참
+////
+//        int num1 = 3;
+//
+//        Test test = new Test();
+//        test.isEven(num1);
+//
+//        System.out.println("num1 = " + num1);
+//
+//        System.out.println("num1 = " + test.isEven(num1));
+//    }
+//
+//    public String isEven(int A) {
+//
+//        return((A % 2) == 0) ? "참" : "거짓";
+//
+//    }
+//}
 
-        String C = (A >= 8) ? "출발 확정" : "출발 보류";
-        String D = (B >= 4) ? "출발 확정" : "출발 보류";
+//}
 
-        System.out.println("A 패키지 : " + A + " 출발 : " + C);
-        System.out.println("B 패키지 : " + B + " 출발 : " + D);
 
-        int sum = A + B;
+        /* 다음 조건을 만족하는 프로그램을 작성하시오.
+         *   구현 클래스 Calculator, Application01
+         *   구현 내용
+         *   Calculator
+         *   - checkMethod(): void 설명 : 함수 호출, 확인용 메소드
+         *   - sumTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 합을 리턴한다.
+         *   - minusTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 차를 리턴한다.
+         *   - multiTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 곱한 값을 리턴한다.
+         *   - divideTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 나눈 값을 리턴한다.
+         *
+         *   Application01
+         *   - main(args:String[]): void 설명 : 모든 메소드는 main 함수에서 호출하여 출력한다.
+         *   // 메소드 호출 확인용 메소드 호출
+         *   // 10, 20 두 개의 정수를 매개변수로 하여 두 수를 더하는 메소드 호출 후 리턴값 출력
+         *   // 10, 5 두 개의 정수를 매개변수로 하여 두 수의 차를 구하는 메소드 호출 후 리턴값 출력
+         *   // 10, 5 두 개의 정수를 매개변수로 하여 두 수의 곱을 구하는 메소드 호출 후 리턴값 출력
+         *   // 10, 5 두 개의 정수를 매개변수로 하여 두 수의 몫을 구하는 메소드 호출 후 리턴값 출력
+         *   실행 결과
+         *   - 메소트 호출 확인
+         *   - 10과 20의 합 : 30
+         *   - 10과 5의 차 : 5
+         *   - 10과 5의 곱 : 50
+         *   - 10과 5의 나눈 후 몫 : 2
+         *  */
 
-        String E = (sum >= 8) ? "출발 확정" : "출발 보류";
-
-        System.out.println("기준 미달이였지만 합산한 결과 : " + E);
+//        int a = 10;
+//        int b = 20;
+//
+//        int c = 10;
+//        int d = 5;
+//
+//
+//        Calc abcd = new Calc();
+//        abcd.chekMethod();
+//        abcd.sumTwoNumber(a, b);
+//        abcd.minusTwoNumber(c, d);
+//        abcd.multiTwoNumber(c, d);
+//        abcd.divideTwoNumber(c, d);
+//
+//        int sum = abcd.sumTwoNumber(a, b);
+//        int minus = abcd.minusTwoNumber(c, d);
+//        int multi = abcd.multiTwoNumber(c, d);
+//        int divide = abcd.divideTwoNumber(c, d);
+//        System.out.println("10과 20의 합 : " + sum);
+//        System.out.println("10과 5의 차 : " + minus);
+//        System.out.println("10과 5의 곱 : " + multi);
+//        System.out.println("10과 5의 몫 : " + divide);
+//
+//    }
+//}
+//        현승이는 User(클래스)에서 현재 통장의 총액을 출력 받고 싶어 합니다.
+//
+//                현승이의 월급은 300인데 월세 60, 통신비 5, 보험료 15를 납부하고 보너스로 20을 받았습니다.
+//
+//        그런데 현승이는 은행 이벤트에 당첨되어 현재까지 있던 통장 잔액이 1.1배가 되었습니다.
+//
+//        (통장 잔액의 시작은 0입니다.)
+//
+//        현재 현승이의 통장 잔액은 얼마인가요?
+//
+//                통장 잔액을 출력 받을 수 있는 코드를 작성해주세요.
+//
+//                클래스 : User, Account
+//
+//        User 클래스에는 계산 메서드가 없습니다.
+//
+//                계산 메서드는 Account에 있습니다.
+//
+//                원하는 출력 형태:
+//
+//        현재 현승이의 통장 잔액은 *****원 입니다.
 
 
     }
-
 }
